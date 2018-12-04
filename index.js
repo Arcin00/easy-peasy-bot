bec43fa941786230ab5bfcf29ef748e13f7e1527
Haris Siddiqui <hsiddiqui@equalexperts.com>
2018-12-04 13:42:30 +0000

    Uncommented timestamp message

--- index.js
+++ index.js
@@ -101,15 +101,15 @@ controller.on('channel_join', function(bot, message) {
  * AN example of what could be:
  * Any un-handled direct mention gets a reaction and a pat response!
  */
-//controller.on('direct_message,mention,direct_mention', function (bot, message) {
-//    bot.api.reactions.add({
-//        timestamp: message.ts,
-//        channel: message.channel,
-//        name: 'robot_face',
-//    }, function (err) {
-//        if (err) {
-//            console.log(err)
-//        }
-//        bot.reply(message, 'I heard you loud and clear boss.');
-//    });
-//});
+controller.on('direct_message,mention,direct_mention', function (bot, message) {
+    bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'robot_face',
+    }, function (err) {
+        if (err) {
+            console.log(err)
+        }
+        bot.reply(message, 'I heard you loud and clear boss.');
+    });
+});
